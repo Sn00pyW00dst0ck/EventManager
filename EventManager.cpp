407f9b75164bee2c3ea47b619458e57205a3c280
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-12-12 19:15:06 -0500

    Adding Basic Functionality

--- EventManager.cpp
+++ EventManager.cpp
@@ -1,1 +1,99 @@
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
 #include "EventManager.h"
+
+EventManager::Event::Event(void)  {
+    this->eventType = EVENT_NONE;
+}
+
+void EventManager::Event::update(void)  {
+    this->update(millis());
+}
+
+void EventManager::Event::update(unsigned long now)  {
+	// Check if event should update
+    if (now - this->lastEventTime >= period)  {
+		// Do different stuff for different events
+		switch (eventType)  {
+			case EVENT_EVERY:
+				(*this->callback)();
+				break;
+			case EVENT_OSCILLATE:
+				this->pinState = !this->pinState;
+				digitalWrite(this->pin, this->pinState);
+				break;
+            case EVENT_DIGITAL_READ:
+
+                break;
+            case EVENT_ANALOG_READ:
+
+                break;
+			case EVENT_NONE:
+				return;
+		}
+		this->lastEventTime = now;
+		this->count++;
+	}
+	// Check if we did the event the number of requested times
+	if (this->repeatCount > -1 && this->count >= this->repeatCount)  {
+		this->eventType = EVENT_NONE;
+	}
+}
+
+int8_t EventManager::executeEvery(unsigned long period, void (*callback)(void))  {
+    return this->executeEvery(period, callback, -1);
+}
+
+int8_t EventManager::executeEvery(unsigned long period, void (*callback)(void), int repeatCount)  {
+    int8_t i = findFreeEventIndex();
+	if (i == NO_TIMER_AVAILABLE) return NO_TIMER_AVAILABLE;
+
+	// Set the event properties for executing every X millis
+	this->_events[i].eventType = EVENT_EVERY;
+	this->_events[i].period = period;
+	this->_events[i].repeatCount = repeatCount;
+	this->_events[i].callback = callback;
+	this->_events[i].lastEventTime = millis();
+	this->_events[i].count = 0;
+	return i;
+}
+
+int8_t EventManager::executeAfter(unsigned long duration, void (*callback)(void))  {
+    return this->executeEvery(duration, callback, 1);
+}
+
+void EventManager::stop(int8_t id)  {
+	// Stop the requested event from happening from now on
+    if (id >= 0 && id < MAX_NUMBER_OF_EVENTS) {
+		this->_events[id].eventType = EVENT_NONE;
+	}
+}
+
+void EventManager::update(void)  {
+    unsigned long now = millis();
+	this->update(now);
+}
+
+void EventManager::update(unsigned long now)  {
+	// Update all the events with the given time in millis
+    for (int8_t i = 0; i < MAX_NUMBER_OF_EVENTS; i++)  {
+		if (this->_events[i].eventType != EVENT_NONE)  {
+			this->_events[i].update(now);
+		}
+	}
+}
+
+int8_t EventManager::findFreeEventIndex(void)  {
+	// Find an event listed as NONE and return it for use later
+	for (int8_t i = 0; i < MAX_NUMBER_OF_EVENTS; i++)  {
+		if (this->_events[i].eventType == EVENT_NONE)  {
+			return i;
+		}
+	}
+	return NO_TIMER_AVAILABLE;
+}
--- EventManager.h
+++ EventManager.h
@@ -1,5 +1,17 @@
+#ifndef event_manager
+#define event_manager
+
 #include <inttypes.h>
 #include <Arduino.h>
+
+#define EVENT_NONE 0
+#define EVENT_EVERY 1
+#define EVENT_OSCILLATE 2
+#define EVENT_DIGITAL_READ 3
+#define EVENT_ANALOG_READ 4
+#define MAX_NUMBER_OF_EVENTS 10
+#define TIMER_NOT_AN_EVENT -2
+#define NO_TIMER_AVAILABLE -1
 
 class EventManager  {
     private:
@@ -18,15 +30,15 @@ class EventManager  {
         int repeatCount;
 
         // callback
-        template<typename ...Arguments>
-        void (*callback)(...Arguments);
+        void (*callback)(void);
+
         Event(void);
 
         void update(void);
         void update(unsigned long now);
     };
 
-    EventManager::Event _events[10];
+    EventManager::Event _events[MAX_NUMBER_OF_EVENTS];
     int8_t findFreeEventIndex(void);
 
     public:
@@ -34,11 +46,10 @@ class EventManager  {
     int8_t executeEvery(unsigned long period, void (*callback)(void));
     int8_t executeEvery(unsigned long period, void (*callback)(void), int repeatCount);
     int8_t executeAfter(unsigned long duration, void (*callback)(void));
-    int8_t oscillateEvery(uint8_t pin, unsigned long period, uint8_t startingValue);
-    int8_t oscillateEvery(uint8_t pin, unsigned long period, uint8_t startingValue, int repeatCount);
-    int8_t readEvery();
-    
+
     void stop(int8_t id);
     void update(void);
     void update(unsigned long now);
 };
+
+#endif
